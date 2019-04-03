b45a0d74208a26fd1eabfb2744a2e922807b2e88
Chris Nateghi <c.nateghi@pinpoint-solutions.nl>
2019-04-03 15:14:34 +0200

    Update HttpLoadRunner.cs

--- HttpLoadRunner.cs
+++ HttpLoadRunner.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace Sita.BagDrop.DcsConnect.LoadTests.Helpers
+namespace LoadTests.Helpers
 {
     public class HttpLoadRunner
     {
